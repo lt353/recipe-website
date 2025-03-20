59f86caa878f71c5c375211caf3c6c2f70fc0918
Lindsay Trenton <lt353@hawaii.edu>
2025-03-19 14:07:03 -1000

    Changes to README.md, recipes.js turned into recipes.json and recipes added, lots of changes to index.js and all the partials except for the footer, changes to the style sheet. More detailed changes are listed in the README.

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # recipe-website
-I am still working on this but so far I have a bare bones website with 4 webpages: 
+This is a bare bones website with 4 webpages: 
   - Homepage
   - Recipe 1
   - Recipe 2
   - Recipe 3
 
-There is a navigation bar in the header which is available throughout all the webpages. There is also a footer available  as well on every page.
+There is a navigation bar in the header which is available throughout all the webpages, though on the homepage it will just be welcome text. In each recipe page it will contain links to the homepage and the 2 other recipes. 
+There is also a footer available  as well on every page.
 
-I've created some very rough styling and content. No actual recipes yet and the styling needs a lot of work, though I like the picture in the header. 
+I am basing the layout on this base figma wirefram: https://www.figma.com/design/8h04jJjVaL8BlXTsjqDVr5/Recipe-Website-wireframe?node-id=0-1&m=dev&t=tz2bUddGlKwR2MFJ-1 
 
-# Recently completed: 
+# Completed previously: 
 - Improved navigation
   - Fixed the background (lightened)
   - Adjusted spacing between the items.
@@ -25,11 +26,28 @@ I've created some very rough styling and content. No actual recipes yet and the 
 - Found images for the recipes (not added into content yet)
 
 
-# Still to-do: 
-- Insert the actual recipes in recipes.js
-  - Ingredients (as a list)
+# Recently completed: 
+- Insert the actual recipes in recipes.json
+  - Ingredients (as an array with quantity, unit, ingredient for each)
   - Step-by-step insstructions
-- Update recipes.ejs to diplay the ingredients and instructions for the remaining recipes
-- Insert pictures (store in /public/images/recipe(#).jpg)
-  - Update recipes.js to include the image paths
-- Maybe add more of a writeup above the recipes on the homepage. 
+  - I decided to go with json as we learned about it and if this ever evolved as a more dynamic webpage with a lot more recipes, JSON is the way to go. 
+- Updated recipes.ejs to display the ingredients and instructions for the remaining recipes
+- Inserted pictures (stored in /public/images/recipe(#).jpg)
+  - Updated recipes.js to include the image paths
+- Added more of a writeup above the recipes on the homepage. 
+- I adjusted the navigation bar so that it changes depending on what recipe we are on. 
+- I changed the homepage a bit
+  - More of an intro
+  - The recipe links are now small images and recipe titles in card/grid format, both of which are clickable. 
+- I added a print button
+  - In CSS I made changes so that when printing it only prints out the recipe, no header, footer, buttons, images. 
+- I made a lot of changes to CSS to account for the new content. 
+
+# Possible future changes
+Although this now meets the project requirements, there are some things I could add in the future to make this a more dynamic website: 
+  - Metric/Imperial conversion. There is a npm called convert-units that can help with this. 
+  - Serving size changes. Create a function to scale the ingredient amounts up or down. 
+  - For both of the ideas above there is something called fraction.js that will come in handy to make this work that is an npm that can be imported. 
+  - Let user add recipes
+    - I found an npm called ingredient-parser which can be used to turn long ingredient lists that users enter and turn it into structured JSON, handy. 
+    - I found the multer npm can be used to allow users to add images to the recipes they add
--- index.js
+++ index.js
@@ -5,9 +5,14 @@
  * Description: 
  * - This is a simple website with a homepage that displays a list of 3 recipes, each 
  *   with their own url. 
- * - There are headers and footers that remain consistent across all pages.
+ * - There are headers and footers that remain consistent across all pages, though the 
+ *   links adjust dynamically depending on what page you are on.
+ * - There is the ability to print the recipes, without all the extra things like the
+ *   header, footer, images, etc. Just the recipe itself.
  * - The website uses EJS to render the views.
  * - The website uses Express to handle the routes.
+ * - The website uses fs to read the recipes from a JSON file.
+ * - The website uses a custom CSS file for styling.
  *   
  * Author: Lindsay Trenton 
  * Date: February 2025
@@ -15,51 +20,81 @@
  * External Packages Used:
  * - express (http://www.npmjs.com/package/express)
  * - ejs (https://www.npmjs.com/package/ejs)
+ * - fs (https://www.npmjs.com/package/fs)
  *
  * Images found on Unsplash, specifically from: 
  * - Brooke Lark for header & background images (https://unsplash.com/@brookelark)
  * - Nick Varvounis for the thai curry (https://unsplash.com/@nickkarvounis)
  * 
- * And images from: 
+ * And other images from: 
  * - https://theflavoursofkitchen.com/ for the crispy chicken tacos
  * - https://sanitasbynikki.com/ for the chicken marsala
  */
 
 // Importing the express package
 const express = require("express");
+// Importing the fs package to read JSON files, specifically the recipes.json file
+const fs = require("fs");
 // Creating an instance of the express package
 const app = express();
-// Importing the recipes.js file
-const recipes = require("./recipes");
+// Path to the recipes.json file
+const FILE_PATH = "./recipes.json";
+// Port number for the server
+const PORT = 3000;
 
 // Setting the view engine to EJS
 app.set("view engine", "ejs");
 
-// Setting the public folder to be used for static files
+// Setting the public folder to be used for static files, which include the images and CSS
 app.use(express.static("public"));
 
-// Home Route
+/*
+ * Function: loadRecipes
+ * Reads the recipes from recipes.json and returns them as an array.
+ * If the file does not exist, it returns an empty array.
+ */
+const loadRecipes = () => {
+    if (fs.existsSync(FILE_PATH)) {
+        // Reads from the recipes array in the recipes.json file    
+        return JSON.parse(fs.readFileSync(FILE_PATH, "utf8")).recipes;
+    }
+    return [];
+};
+
+/* 
+ * Route: Homepage
+ * - Displays only a small image and tile for each recipes
+ * - Botht he image and the title will be clickable
+ */
 app.get("/", function (req, res) {
+    const recipes = loadRecipes(); // Loads recipes dynamically
     res.render("index", {
         title: "Welcome to My Recipes",
         recipes: recipes //passing the recipes array to the index.ejs file
     });
 });
+
 /*
- *Recipe Routes 
- *The recipes are stored in an array in the recipes.js file
- *Each recipe has a title and content property
- *The title is displayed as a link on the home page
- *This is done with a loop to generate the links dynamically
+ * Route: Individual Recipe Pages
+ * - Each recipe has its own URL
+ * - Each recipe has a title, image, ingredients, instructions, and print button
+ * - the title is used in the URL and is passed to the EJS file
  */
-recipes.forEach((recipe, index) => {
-    app.get(`/recipe${index + 1}`, (req, res) => {
-        res.render("recipes", {
-            recipe: recipe // Passing the entire recipe object. I tried passing just the title and content properties, but it didn't work.
-        });
+app.get("/recipe/:title", (req, res) => {
+    const recipes = loadRecipes(); // Loads recipes dynamically
+    const recipe = recipes.find(recipe => recipe.title.toLowerCase() === req.params.title.toLowerCase());
+
+    if (!recipe) {
+        res.status(404).send("Recipe not found.");
+    }
+    // Render the full recipe page with the recipe object
+    res.render("recipes", {
+        recipe: recipe,
+        recipes: recipes, //needed for navigation in the header
+        currentPage: recipe.title // Identifies the current recipe page for header.ejs
     });
 });
 
-app.listen(3000, function () {
-    console.log("Server is running on port 3000");
+app.listen(PORT, function () {
+    console.log(`Server is running on port ${PORT}`);
 });
